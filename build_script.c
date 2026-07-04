3cf27bfced16b526de623e9589d2006111f90367
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2026-07-04 14:25:28 -0700

    Almost got the samples executable compiling

--- build_script.c
+++ build_script.c
@@ -8,7 +8,10 @@ Description:
 
 #include "pig_build.h"
 
-#define DEBUG_BUILD 1
+#define DEBUG_BUILD                  1
+#define BUILD_BOX3D_DLL              0
+#define BUILD_SAMPLES                1
+#define ONLY_BUILD_NON_EXISTANT_OBJS 0
 
 #define BOX3D_SOURCE_FILES {          \
 	"[ROOT]/src/aabb.c",              \
@@ -108,19 +111,58 @@ Description:
 	"[ROOT]/include/box3d/types.h",          \
 }
 
-#define BOX3D_SHARED_FILES { \
-	"benchmarks.c",          \
-	"benchmarks.h",          \
-	"determinism.c",         \
-	"determinism.h",         \
-	"human.c",               \
-	"human.h",               \
-	"overflow_color.c",      \
-	"overflow_color.h",      \
-	"stability.c",           \
-	"stability.h",           \
-	"utils.c",               \
-	"utils.h",               \
+#define BOX3D_SHARED_FILES {               \
+	"[ROOT]/shared/benchmarks.c",          \
+	"[ROOT]/shared/benchmarks.h",          \
+	"[ROOT]/shared/determinism.c",         \
+	"[ROOT]/shared/determinism.h",         \
+	"[ROOT]/shared/human.c",               \
+	"[ROOT]/shared/human.h",               \
+	"[ROOT]/shared/overflow_color.c",      \
+	"[ROOT]/shared/overflow_color.h",      \
+	"[ROOT]/shared/stability.c",           \
+	"[ROOT]/shared/stability.h",           \
+	"[ROOT]/shared/utils.c",               \
+	"[ROOT]/shared/utils.h",               \
+}
+
+#define SAMPLE_FILES {                       \
+	"[ROOT]/samples/main.cpp",               \
+	/*Host*/                                 \
+	"[ROOT]/samples/host/camera.cpp",        \
+	"[ROOT]/samples/host/camera.h",          \
+	"[ROOT]/samples/host/gui.cpp",           \
+	"[ROOT]/samples/host/gui.h",             \
+	"[ROOT]/samples/host/imgui_impl.cpp",    \
+	"[ROOT]/samples/host/sokol_app_impl.c",  \
+	"[ROOT]/samples/sample.cpp",             \
+	"[ROOT]/samples/sample.h",               \
+	"[ROOT]/samples/mesh_loader.cpp",        \
+	"[ROOT]/samples/mesh_loader.h",          \
+	"[ROOT]/samples/jsmn.h",                 \
+	"[ROOT]/samples/tiny_obj_loader.h",      \
+	"[ROOT]/samples/earcut.h",               \
+	/*Samples*/                              \
+	"[ROOT]/samples/sample_benchmark.cpp",   \
+	"[ROOT]/samples/sample_bodies.cpp",      \
+	"[ROOT]/samples/sample_character.cpp",   \
+	"[ROOT]/samples/sample_collision.cpp",   \
+	"[ROOT]/samples/sample_compound.cpp",    \
+	"[ROOT]/samples/sample_continuous.cpp",  \
+	"[ROOT]/samples/sample_determinism.cpp", \
+	"[ROOT]/samples/sample_events.cpp",      \
+	"[ROOT]/samples/sample_geometry.cpp",    \
+	"[ROOT]/samples/sample_issues.cpp",      \
+	"[ROOT]/samples/sample_joint.cpp",       \
+	"[ROOT]/samples/sample_manifold.cpp",    \
+	"[ROOT]/samples/sample_mesh.cpp",        \
+	"[ROOT]/samples/sample_ragdoll.cpp",     \
+	"[ROOT]/samples/sample_replay.cpp",      \
+	"[ROOT]/samples/sample_robustness.cpp",  \
+	"[ROOT]/samples/sample_shapes.cpp",      \
+	"[ROOT]/samples/sample_stacking.cpp",    \
+	"[ROOT]/samples/sample_tree.cpp",        \
+	"[ROOT]/samples/sample_world.cpp",       \
 }
 
 #if DEBUG_BUILD
@@ -152,34 +194,122 @@ Description:
 #define IF_NOT_OSX(...) __VA_ARGS__
 #endif
 
+#define T_SAMPLES   "|Samples"
+#define T_BOX3D_DLL "|Box3Ddll"
+
+void DownloadDearImgui();
+void DownloadImplot();
+void DownloadNativeFileDialog();
+
+bool IsCompilableExt(Str pathStr)
+{
+	Str extension = GetFileExtPart(pathStr, false);
+	return (
+		StrAnyCaseEquals(extension, StrLit(".c")) ||
+		StrAnyCaseEquals(extension, StrLit(".cpp")) ||
+		StrAnyCaseEquals(extension, StrLit(".m")) ||
+		StrAnyCaseEquals(extension, StrLit(".mm"))
+	);
+}
+bool IsCppExt(Str pathStr)
+{
+	Str extension = GetFileExtPart(pathStr, false);
+	return (
+		StrAnyCaseEquals(extension, StrLit(".cpp")) ||
+		StrAnyCaseEquals(extension, StrLit(".mm"))
+	);
+}
+
+void MakeSureObjectIsBuilt(Str sourcePath, Str objectPath, CliArgs* compilerFlags, StrArray* commonTags, const char* extraTag)
+{
+	if (!ONLY_BUILD_NON_EXISTANT_OBJS || !DoesFileExist(objectPath))
+	{
+		Str sourceFileName = GetFileNamePart(sourcePath, true);
+		PrintLine("[Compiling... %.*s -> %.*s]", StrPrint(sourceFileName), StrPrint(objectPath));
+		
+		CliArgs args = EMPTY;
+		AddArgStr(&args, CLI_QUOTED_ARG, sourcePath);
+		AddTaggedArg(&args, T_MSVC_CL, CL_COMPILE);
+		AddTaggedArg(&args, T_CLANG,   CLANG_COMPILE);
+		AddTaggedArgStr(&args, T_MSVC_CL, CL_OBJ_FILE, objectPath);
+		AddTaggedArgStr(&args, T_CLANG,   CLANG_OUTPUT_FILE, objectPath);
+		AddArgList(&args, compilerFlags);
+		
+		StrArray tags = EMPTY;
+		AddTag(&tags, T_OBJECT);
+		if (extraTag != nullptr) { AddTag(&tags, extraTag); }
+		AddTag(&tags, IsCppExt(sourceFileName) ? T_LANG_CPP : T_LANG_C);
+		AddStrArray(&tags, commonTags);
+		IF_WINDOWS(AddTag(&tags, T_MSVC_CL));
+		IF_WINDOWS(AddTag(&tags, T_MSVC_CL_OR_LINK));
+		IF_NOT_WINDOWS(AddTag(&tags, T_CLANG));
+		
+		Str compilerExe = BUILDING_ON_WINDOWS ? StrLit(EXE_MSVC_CL) : StrLit(EXE_CLANG);
+		RunCliProgramAndExitOnFailureTags(compilerExe, tags, &args, FormatStr("Failed to compile \"%.*s\"", StrPrint(sourceFileName)));
+		AssertFileExist(objectPath, true);
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	RecompileIfNeeded(StrArray_Empty);
 	#if BUILDING_ON_WINDOWS
 	bool isMsvcInitialized = WasMsvcDevBatchRun();
 	#endif
-	const char* sourcePathsNt[] = BOX3D_SOURCE_FILES;
 	const char* sharedPathsNt[] = BOX3D_SHARED_FILES;
+	const char* dllSourcePathsNt[] = BOX3D_SOURCE_FILES;
 	const char* includePathsNt[] = BOX3D_INCLUDE_FILES;
+	const char* samplesSourcePathsNt[] = SAMPLE_FILES;
 	Str box3dDllPath = StrLit("box3d" DLL_EXT);
+	Str box3dLibPath = StrLit("box3d" LIB_EXT);
+	Str samplesExePath = StrLit("box3d_samples" EXE_EXT);
+	IF_WINDOWS(Str box3dPdbPath = StrLit("box3d.pdb"));
+	
+	DownloadDearImgui();
+	DownloadImplot();
+	DownloadNativeFileDialog();
 	
 	CliArgs compilerFlags = EMPTY;
 	CliArgs linkerFlags = EMPTY;
 	AddTaggedArg(&compilerFlags,    T_MSVC_CL, CL_FULL_FILE_PATHS);
 	AddTaggedArg(&compilerFlags,    T_MSVC_CL, CL_NO_LOGO);
 	AddTaggedArg(&linkerFlags,      T_MSVC_CL, LINK_DISABLE_INCREMENTAL);
+	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL, CL_CONFIGURE_EXCEPTION_HANDLING, "s"); //enable stack-unwinding
+	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL, CL_CONFIGURE_EXCEPTION_HANDLING, "c"); //extern "C" functions don't through exceptions
 	AddTaggedArg(&compilerFlags,    T_CLANG,   CLANG_FULL_FILE_PATHS);
 	
 	AddTaggedArgNt(&compilerFlags,  T_CLANG,   CLANG_INCLUDE_DIR, "[ROOT]/include");
 	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL, CL_INCLUDE_DIR,    "[ROOT]/include");
+	AddTaggedArgNt(&compilerFlags,  T_CLANG,   CLANG_INCLUDE_DIR, "[ROOT]/shared");
+	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL, CL_INCLUDE_DIR,    "[ROOT]/shared");
 	AddTaggedArgNt(&compilerFlags,  T_CLANG,   CLANG_INCLUDE_DIR, "[ROOT]/bin");
 	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL, CL_INCLUDE_DIR,    "[ROOT]/bin");
+	AddTaggedArgNt(&compilerFlags,  T_CLANG,   CLANG_INCLUDE_DIR, "[ROOT]/data");
+	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL, CL_INCLUDE_DIR,    "[ROOT]/data");
+	
+	AddTaggedArgNt(&compilerFlags,  T_CLANG   T_SAMPLES, CLANG_INCLUDE_DIR, "[ROOT]/samples");
+	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL T_SAMPLES, CL_INCLUDE_DIR,    "[ROOT]/samples");
+	AddTaggedArgNt(&compilerFlags,  T_CLANG   T_SAMPLES, CLANG_INCLUDE_DIR, "[ROOT]/extern/sokol");
+	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL T_SAMPLES, CL_INCLUDE_DIR,    "[ROOT]/extern/sokol");
+	AddTaggedArgNt(&compilerFlags,  T_CLANG   T_SAMPLES, CLANG_INCLUDE_DIR, "imgui");
+	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL T_SAMPLES, CL_INCLUDE_DIR,    "imgui");
+	AddTaggedArgNt(&compilerFlags,  T_CLANG   T_SAMPLES, CLANG_INCLUDE_DIR, "implot");
+	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL T_SAMPLES, CL_INCLUDE_DIR,    "implot");
+	AddTaggedArgNt(&compilerFlags,  T_CLANG   T_SAMPLES, CLANG_INCLUDE_DIR, "nfd/src/include");
+	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL T_SAMPLES, CL_INCLUDE_DIR,    "nfd/src/include");
+	
+	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL T_WINDOWS,   CL_DEFINE,    "SOKOL_D3D11");
+	AddTaggedArgNt(&compilerFlags,  T_CLANG   T_LINUX,     CLANG_DEFINE, "SOKOL_GLCORE");
+	AddTaggedArgNt(&compilerFlags,  T_CLANG   T_OSX,       CLANG_DEFINE, "SOKOL_METAL");
+	
+	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL T_BOX3D_DLL, CL_DEFINE,    "box3d_EXPORTS");
+	AddTaggedArgNt(&compilerFlags,  T_CLANG   T_BOX3D_DLL, CLANG_DEFINE, "box3d_EXPORTS");
 	
 	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL T_LANG_C,          CL_LANG_VERSION, "c17");
 	AddTaggedArgNt(&compilerFlags,  T_CLANG   T_LANG_C,          CLANG_LANG_VERSION, "gnu17");
 	AddTaggedArgNt(&compilerFlags,  T_CLANG   T_LANG_OBJECTIVEC, CLANG_LANG_VERSION, "gnu17");
-	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL T_LANG_CPP,        CL_LANG_VERSION, "c++17");
-	AddTaggedArgNt(&compilerFlags,  T_CLANG   T_LANG_CPP,        CLANG_LANG_VERSION, "c++17");
+	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL T_LANG_CPP,        CL_LANG_VERSION, "c++20");
+	AddTaggedArgNt(&compilerFlags,  T_CLANG   T_LANG_CPP,        CLANG_LANG_VERSION, "c++20");
 	AddTaggedArgNt(&compilerFlags,  T_CLANG   T_LANG_CPP,        CLANG_SYSTEM_LIBRARY, "stdc++");
 	AddTaggedArg(&compilerFlags,    T_CLANG   T_LANG_OBJECTIVEC, CLANG_ENABLE_OBJC_ARC);
 	
@@ -194,77 +324,90 @@ int main(int argc, char* argv[])
 	AddTaggedArgNt(&compilerFlags,  T_CLANG T_UNIX T_RELEASE_BUILD, CLANG_OPTIMIZATION_LEVEL, "2");
 	AddTaggedArgNt(&compilerFlags,  T_CLANG T_UNIX T_DEBUG_BUILD,   CLANG_DEBUG_INFO, "dwarf-4");
 	
-	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL, CL_WARNING_LEVEL, "X");
-	AddTaggedArgNt(&compilerFlags,  T_MSVC_CL, CL_WARNING_LEVEL, "4");
-	AddTaggedArgNt(&compilerFlags,  T_CLANG,   CLANG_WARNING_LEVEL, "all");
-	AddTaggedArgNt(&compilerFlags,  T_CLANG,   CLANG_WARNING_LEVEL, "extra");
+	// AddTaggedArgNt(&compilerFlags,  T_MSVC_CL, CL_WARNING_LEVEL, "X");
+	// AddTaggedArgNt(&compilerFlags,  T_MSVC_CL, CL_WARNING_LEVEL, "4");
+	// AddTaggedArgNt(&compilerFlags,  T_CLANG,   CLANG_WARNING_LEVEL, "all");
+	// AddTaggedArgNt(&compilerFlags,  T_CLANG,   CLANG_WARNING_LEVEL, "extra");
 	
 	StrArray commonTags = EMPTY;
-	AddTag(&commonTags, T_LANG_C);
 	IF_DEBUG(AddTag(&commonTags, "DEBUG_BUILD"));
-	
 	IF_WINDOWS(AddTag(&commonTags, T_WINDOWS));
 	IF_LINUX(AddTag(&commonTags, T_LINUX));
 	IF_OSX(AddTag(&commonTags, T_OSX));
 	IF_NOT_WINDOWS(AddTag(&commonTags, T_UNIX));
 	
 	// +==============================+
-	// |           Compile            |
+	// |    Compile Shared Objects    |
 	// +==============================+
-	StrArray objectPaths = EMPTY;
-	for (u64 fIndex = 0; fIndex < ArrayCount(sourcePathsNt); fIndex++)
+	#if (BUILD_BOX3D_DLL || BUILD_SAMPLES)
+	StrArray sharedObjectPaths = EMPTY;
+	for (u64 fIndex = 0; fIndex < ArrayCount(sharedPathsNt); fIndex++)
 	{
-		Str sourcePath = MakeStrNt(sourcePathsNt[fIndex]);
-		Str sourceExt = GetFileExtPart(sourcePath, false);
-		if (StrAnyCaseEquals(sourceExt, StrLit(".c")) ||
-			StrAnyCaseEquals(sourceExt, StrLit(".cpp")) ||
-			StrAnyCaseEquals(sourceExt, StrLit(".m")) ||
-			StrAnyCaseEquals(sourceExt, StrLit(".mm")))
+		Str sourcePath = MakeStrNt(sharedPathsNt[fIndex]);
+		if (IsCompilableExt(sourcePath))
 		{
-			Str sourceFileName = GetFileNamePart(sourcePath, true);
-			Str objectPath = ChangePathExtension(sourceFileName, StrLit(OBJ_EXT), true);
-			PrintLine("[Compiling... %.*s -> %.*s]", StrPrint(sourceFileName), StrPrint(objectPath));
-			
-			CliArgs args = EMPTY;
-			AddArgStr(&args, CLI_QUOTED_ARG, sourcePath);
-			AddTaggedArg(&args, T_MSVC_CL, CL_COMPILE);
-			AddTaggedArg(&args, T_CLANG,   CLANG_COMPILE);
-			AddTaggedArgStr(&args, T_MSVC_CL, CL_OBJ_FILE, objectPath);
-			AddTaggedArgStr(&args, T_CLANG,   CLANG_OUTPUT_FILE, objectPath);
-			AddArgList(&args, &compilerFlags);
-			
-			StrArray tags = EMPTY;
-			AddTag(&tags, T_OBJECT);
-			AddStrArray(&tags, &commonTags);
-			IF_WINDOWS(AddTag(&tags, T_MSVC_CL));
-			IF_WINDOWS(AddTag(&tags, T_MSVC_CL_OR_LINK));
-			IF_NOT_WINDOWS(AddTag(&tags, T_CLANG));
-			
-			Str compilerExe = BUILDING_ON_WINDOWS ? StrLit(EXE_MSVC_CL) : StrLit(EXE_CLANG);
-			RunCliProgramAndExitOnFailureTags(compilerExe, tags, &args, FormatStr("Failed to compile \"%.*s\"", StrPrint(sourceFileName)));
-			AssertFileExist(objectPath, true);
-			
-			AddStr(&objectPaths, objectPath);
+			Str objectPath = ChangePathExtension(GetFileNamePart(sourcePath, true), StrLit(OBJ_EXT), true);
+			MakeSureObjectIsBuilt(sourcePath, objectPath, &compilerFlags, &commonTags, nullptr);
+			AddStr(&sharedObjectPaths, objectPath);
 		}
 	}
+	#endif //(BUILD_BOX3D_DLL || BUILD_SAMPLES)
 	
 	// +==============================+
-	// |             Link             |
+	// |     Compile DLL Objects      |
 	// +==============================+
+	#if BUILD_BOX3D_DLL
+	StrArray dllObjectPaths = EMPTY;
+	for (u64 fIndex = 0; fIndex < ArrayCount(dllSourcePathsNt); fIndex++)
+	{
+		Str sourcePath = MakeStrNt(dllSourcePathsNt[fIndex]);
+		if (IsCompilableExt(sourcePath))
+		{
+			Str objectPath = ChangePathExtension(GetFileNamePart(sourcePath, true), StrLit(OBJ_EXT), true);
+			MakeSureObjectIsBuilt(sourcePath, objectPath, &compilerFlags, &commonTags, T_BOX3D_DLL);
+			AddStr(&dllObjectPaths, objectPath);
+		}
+	}
+	#endif //BUILD_BOX3D_DLL
+	
+	// +==============================+
+	// |    Compile Sample Objects    |
+	// +==============================+
+	#if BUILD_SAMPLES
+	StrArray samplesObjectPaths = EMPTY;
+	for (u64 fIndex = 0; fIndex < ArrayCount(samplesSourcePathsNt); fIndex++)
+	{
+		Str sourcePath = MakeStrNt(samplesSourcePathsNt[fIndex]);
+		if (IsCompilableExt(sourcePath))
+		{
+			Str objectPath = ChangePathExtension(GetFileNamePart(sourcePath, true), StrLit(OBJ_EXT), true);
+			MakeSureObjectIsBuilt(sourcePath, objectPath, &compilerFlags, &commonTags, T_SAMPLES);
+			AddStr(&samplesObjectPaths, objectPath);
+		}
+	}
+	#endif //BUILD_SAMPLES
+	
+	// +==============================+
+	// |        Link box3d.dll        |
+	// +==============================+
+	#if BUILD_BOX3D_DLL
 	{
 		PrintLine("[Linking %.*s...]", StrPrint(box3dDllPath));
 		
 		CliArgs args = EMPTY;
-		for (u64 oIndex = 0; oIndex < objectPaths.length; oIndex++) { AddArgStr(&args, CLI_QUOTED_ARG, objectPaths.strings[oIndex]); }
 		AddTaggedArg(&args,    T_MSVC_LINK, LINK_BUILD_DLL);
 		AddTaggedArg(&args,    T_CLANG,     CLANG_BUILD_SHARED_LIB);
+		AddTaggedArg(&args,    T_CLANG,     CLANG_fPIC);
 		AddTaggedArgStr(&args, T_MSVC_LINK, LINK_OUTPUT_FILE, box3dDllPath);
 		AddTaggedArgStr(&args, T_CLANG,     CLANG_OUTPUT_FILE, box3dDllPath);
 		AddArgList(&args, &compilerFlags);
 		AddArgList(&args, &linkerFlags);
+		for (u64 oIndex = 0; oIndex < sharedObjectPaths.length; oIndex++) { AddArgStr(&args, CLI_QUOTED_ARG, sharedObjectPaths.strings[oIndex]); }
+		for (u64 oIndex = 0; oIndex < dllObjectPaths.length; oIndex++) { AddArgStr(&args, CLI_QUOTED_ARG, dllObjectPaths.strings[oIndex]); }
 		
 		StrArray tags = EMPTY;
 		AddTag(&tags, T_LIBRARY);
+		AddTag(&tags, T_BOX3D_DLL);
 		AddStrArray(&tags, &commonTags);
 		IF_WINDOWS(AddTag(&tags, T_MSVC_LINK));
 		IF_WINDOWS(AddTag(&tags, T_MSVC_CL_OR_LINK));
@@ -274,5 +417,97 @@ int main(int argc, char* argv[])
 		RunCliProgramAndExitOnFailureTags(linkerExe, tags, &args, FormatStr("Failed to link \"%.*s\"", StrPrint(box3dDllPath)));
 		AssertFileExist(box3dDllPath, true);
 		PrintLine("[Done Linking %.*s!]", StrPrint(box3dDllPath));
+	}
+	#endif
+	
+	// +==============================+
+	// |       Link Samples.exe       |
+	// +==============================+
+	#if BUILD_SAMPLES
+	{
+		PrintLine("[Linking %.*s...]", StrPrint(samplesExePath));
+		
+		CliArgs args = EMPTY;
+		// AddTaggedArg(&args,    T_MSVC_LINK, LINK_BUILD_DLL);
+		// AddTaggedArg(&args,    T_CLANG,     CLANG_BUILD_SHARED_LIB);
+		// AddTaggedArg(&args,    T_CLANG,     CLANG_fPIC);
+		AddTaggedArgStr(&args, T_MSVC_LINK, LINK_OUTPUT_FILE, samplesExePath);
+		AddTaggedArgStr(&args, T_CLANG,     CLANG_OUTPUT_FILE, samplesExePath);
+		AddArgList(&args, &compilerFlags);
+		AddArgList(&args, &linkerFlags);
+		for (u64 oIndex = 0; oIndex < sharedObjectPaths.length; oIndex++) { AddArgStr(&args, CLI_QUOTED_ARG, sharedObjectPaths.strings[oIndex]); }
+		for (u64 oIndex = 0; oIndex < samplesObjectPaths.length; oIndex++) { AddArgStr(&args, CLI_QUOTED_ARG, samplesObjectPaths.strings[oIndex]); }
+		AddArgStr(&args, CLI_QUOTED_ARG, box3dLibPath);
+		
+		StrArray tags = EMPTY;
+		AddTag(&tags, T_LIBRARY);
+		AddTag(&tags, T_SAMPLES);
+		AddStrArray(&tags, &commonTags);
+		IF_WINDOWS(AddTag(&tags, T_MSVC_LINK));
+		IF_WINDOWS(AddTag(&tags, T_MSVC_CL_OR_LINK));
+		IF_NOT_WINDOWS(AddTag(&tags, T_CLANG));
+		
+		Str linkerExe = BUILDING_ON_WINDOWS ? StrLit(EXE_MSVC_LINK) : StrLit(EXE_CLANG);
+		RunCliProgramAndExitOnFailureTags(linkerExe, tags, &args, FormatStr("Failed to link \"%.*s\"", StrPrint(samplesExePath)));
+		AssertFileExist(samplesExePath, true);
+		PrintLine("[Done Linking %.*s!]", StrPrint(samplesExePath));
+	}
+	#endif
+}
+
+void DownloadDearImgui()
+{
+	Str imguiUrl = StrLit("https://github.com/ocornut/imgui/archive/refs/tags/v1.92.7.zip");
+	Str imguiZipPath = StrLit("imgui_v1.92.7.zip");
+	Str imguiZipRootFolder = StrLit("imgui-1.92.7");
+	Str imguiFolderPath = StrLit("imgui");
+	if (!DoesFileExist(imguiZipPath) || !DoesFolderExist(imguiFolderPath))
+	{
+		PrintLine("Downloading Dear ImGui from \"%.*s\"", StrPrint(imguiUrl));
+		DownloadAndExtractArchive(
+			imguiUrl,
+			imguiZipPath,
+			2247481, 0x223CB155060498F1,
+			imguiFolderPath,
+			imguiZipRootFolder
+		);
+	}
+}
+
+void DownloadImplot()
+{
+	Str implotUrl = StrLit("https://github.com/epezent/implot/archive/refs/tags/v0.17.zip");
+	Str implotZipPath = StrLit("implot-v0.17.zip");
+	Str implotZipRootFolder = StrLit("implot-0.17");
+	Str implotFolderPath = StrLit("implot");
+	if (!DoesFileExist(implotZipPath) || !DoesFolderExist(implotFolderPath))
+	{
+		PrintLine("Downloading ImPlot from \"%.*s\"", StrPrint(implotUrl));
+		DownloadAndExtractArchive(
+			implotUrl,
+			implotZipPath,
+			150841, 0xA40A5F3CACFB33C3,
+			implotFolderPath,
+			implotZipRootFolder
+		);
+	}
+}
+
+void DownloadNativeFileDialog()
+{
+	Str nfdUrl = StrLit("https://github.com/btzy/nativefiledialog-extended/archive/refs/tags/v1.3.0.zip");
+	Str nfdZipPath = StrLit("nativefiledialog-extended-v1.3.0.zip");
+	Str nfdZipRootFolder = StrLit("nativefiledialog-extended-1.3.0");
+	Str nfdFolderPath = StrLit("nfd");
+	if (!DoesFileExist(nfdZipPath) || !DoesFolderExist(nfdFolderPath))
+	{
+		PrintLine("Downloading NativeFileDialog-Extended from \"%.*s\"", StrPrint(nfdUrl));
+		DownloadAndExtractArchive(
+			nfdUrl,
+			nfdZipPath,
+			451462, 0x783D44A34B263389,
+			nfdFolderPath,
+			nfdZipRootFolder
+		);
 	}
 }
